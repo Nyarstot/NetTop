7e0c825a66eccce9abfab046d40af72c243820c0
Nikita Kozlov <Nyarstot@protonmail.com>
2022-03-13 16:17:47 +0300

    Add prolog base for expert system

--- src/com/nyarstot/nettop/backend/main.pl
+++ src/com/nyarstot/nettop/backend/main.pl
@@ -1,86 +1,90 @@
-/* This file is a part of NETTOP expert system.
-*
+/* 
+*  This file is a part of NETTOP expert system.
 *  Author: Kozlov Nikita
+*  Date: 3.12.2022
 */
 
-start_system:- 
+/* BASIC FUNCTIONS */
+
+intro_message:-
     sleep(0.2),
     write('                             NETTOP EXPERT SYSTEM                       '), nl,
     sleep(0.2),
     write('                      PLEASE ANSWER SOME FURTHER QUESTION                 '), nl,nl,nl.
 
-ask(Question):-
-    write(Question), nl,
-    read(Output), nl,
-    ((Output == yes; Output == no))
-    ->
-        assert(yes(Question));
-        assert(no(Question), fail).
+find_topology(Topology):-
+    topology(Topology), !.
 
-:- dynamic yes/1, no/1.
+% Store user answers to be able to track his progress
+:- dynamic(progress/2).
 
-verify(Order):-
-    (yes(Order):-
-    ->
-        true;
-        (no(Order)
-        ->
-            fail;
-            ask(Order)
-        )
-    ).
+% Clear stored user progress
+% reset_answers must always return true; because retract can return either true
+% or false, we fail the first and succeed with the second.
+reset_answers:-
+    retract(progress(_,_)),
+    fail.
+reset_answers.
 
-undo :- retract(yes(_)), fail.
-undo :- retract(no(_)), fail.
-undo. 
+start_system:-
+    intro_message,
+    reset_answers,
+    find_topology(Topology),
+    write('You probably looking for: '),
+    describe(Topology), nl.
 
-/* Hypothesis */
+ask(Question, Answer, Choices):-
+    question(Question),
+    answers(Choices, 0),
+    read(Index),
+    parse(Index, Choices, Response),
+    asserta(progress(Question, Response)),
+    Response = Answer.
 
-choose(bus):- bus, !.
-choose(star):- star, !.
-choose(mesh):- mesh, !.
-choose(peer_to_peer):- peer_to_peer, !.
-choose(ring):- ring, !.
-choose(tree):- tree, !.
-choose(hybrid):- hybrid, !.
-choose(not).
+/* RULES FOR THE KNOWLEDGE BASE */
 
-/* Questions */
+topology(peer_to_peer):-
+    why(home).
 
-quest(home):- verify("Are you going to make home network? ").
-quest(corporate):- verify("Are you going to make corporate network? ").
-quest(compute):- verify("Are you going to make network to compute something? ").
-quest(classroom):- verify("Are you going to make network for computer classroom? ").
-quest(two_computers):- verify("Do you have not more than two computers? ").
-quest(five_computers):- verify("Do you have about five computers? ").
-quest(ten_computers):- verify("Do you have about ten computers? ").
-quest(more_computers):- verify("Do you have more than thirty computers? ").
-quest(each_other):- verify("You need computers to be connected to each other? ").
-quest(central_server):- verify("You need computers to be connected to a central server? ").
-quest(hundred_dollars):- verify("Are you willing to spend about 100$ to setup your network? ").
-quest(three_dollars):- verify("Are you willing to spend about 300$ to setup your network? ").
-quest(eight_dollars):- verify("Are you willing to spend about 800% to setup your network? ").
-quest(thousand_dollars):- verify("Are you willing to spend about 1000$ to setup your network? ").
-quest(ten_meters):- verify("Is the distance you want to run your network cable about 10 meters? ").
-quest(forty_meters):- verify("Is the distance you want to run your network cable about 40 meters? ").
-quest(sixty_meters):- verify("Is the distance you want to run your network cable about 60 meters? ").
-quest(hundred_meters):- verify("Is the distance you want to run your network cable about or more than 100 meters? ").
-quest(yes_expand):- verify("Are you going to expand your network in the future? ").
-quest(coaxial_cable):- verify("Are you going to use coaxial cable in your network? ").
-quest(pair_cable):- verify("Are you going to use twisted pair cable in your network? ").
-quest(fiber_cable):- verify("Are you going to use optic fiber cable in your network? ").
+/* QUESTIONS FOR THE KNOWLEDGE BASE */
 
-/* Knowledge */
+question(why):-
+    write('What type of network suits your needs?'), nl.
 
-bus:-
-    verify(home);
-    verify(classroom),
-    verify(two_computers),
-    verify(hundred_dollars),
-    verify(ten_meters),
-    verify(yes_expand),
-    verify(coaxial_cable).
+/* ANSWERS FOR THE KNOWLEDGE BASE */
 
-peer_to_peer:-
-    verify(home),
+answer(home):-
+    write('Home network').
+answer(corporate):-
+    write('Corporate network').
+answer(compute):-
+    write('Computing network').
+answer(classroom):-
+    write('Classroom network').
 
+/* TOPOLOGY DESCRIPTIONS */
+
+describe(peer_to_peer):-
+    write('Peer to peer network'), nl,
+    write('This is a simple type of network where computers are able to communicate with one another and share what is on or attached to their computer with other users.').
+
+/* ASSIGN AN ANSWER TO QUESTIONS FROM THE KNOWLEDGE BASE */
+
+why(Answer):-
+    progress(why, Answer).
+why(Answer):-
+    \+ progress(why,_),
+    ask(why, Answer, [home, corporate, compute, classroom]).
+
+/* ANSWER OUTPUT */
+answers([],_).
+answers([First|Rest], Index):-
+    write('['), write(Index), write(']'), write(' '), answer(First), nl,
+    NextIndex is Index + 1,
+    answers(Rest, NextIndex).
+
+parse(0, [First|_], First).
+parse(Index, [First|Rest], Response):-
+    Index > 0,
+    NextIndex is Index - 1,
+    parse(NextIndex, Rest, Response).
--- src/com/nyarstot/nettop/backend/questions.pl
+++
@@ -1,6 +0,0 @@
-/* This file is a part of NETTOP expert system.
-*  This file contains all the questions using in system.
-*
-*  Author: Kozlov Nikita
-*/
-
